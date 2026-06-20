7d7e616f5dd898b105e3997aa52a147da6c1f973
galihvirgi <galihvirgipramuyda@gmail.com>
2026-06-20 19:39:15 +0700

    update

--- backend/models/Peminjaman.js
+++ backend/models/Peminjaman.js
@@ -1,35 +1,12 @@
 const mongoose = require('mongoose');
 
 const PeminjamanSchema = new mongoose.Schema({
-    // Penambahan type ObjectId dan ref untuk relasi tabel
-    id_anggota: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Anggota', 
-        required: true 
-    },
-    id_buku: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Buku', 
-        required: true 
-    },
-    id_petugas: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Petugas', 
-        required: true 
-    },
-    tanggal_pinjam: { 
-        type: Date, 
-        required: true 
-    },
-    tanggal_kembali_seharusnya: { 
-        type: Date, 
-        required: true 
-    },
-    status_transaksi: { 
-        type: String, 
-        required: true 
-    }
+    id_anggota: { type: mongoose.Schema.Types.ObjectId, ref: 'Anggota', required: true },
+    id_buku: { type: mongoose.Schema.Types.ObjectId, ref: 'Buku', required: true },
+    id_petugas: { type: mongoose.Schema.Types.ObjectId, ref: 'Petugas', required: true },
+    tanggal_pinjam: { type: Date, required: true },
+    tanggal_kembali_seharusnya: { type: Date, required: true },
+    status_transaksi: { type: String, required: true }
 });
 
-// Pastikan nama modelnya 'Peminjaman' agar sesuai
 module.exports = mongoose.model('Peminjaman', PeminjamanSchema, 'peminjaman');
--- backend/models/Petugas.js
+++ backend/models/Petugas.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PetugasSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true }, // Tambahan baru
+    password: { type: String, required: true },
     nama_petugas: { type: String, required: true },
     jabatan: String,
 });
